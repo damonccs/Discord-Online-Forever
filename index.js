e9421e2ee79c26d596fa99390756770807fec60d
Lyxn <115940004+lyxna@users.noreply.github.com>
2025-01-30 20:17:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const os = require("os");
+const keep_alive = require("./keep_alive.js");
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris(process.env.token, {
+  connectionProperties: {
+    $os: os.platform(), // Detects platform (Windows, Linux, macOS)
+    $browser: "Discord Android", // Makes it appear as a mobile device
+    $device: "Discord Android" // Shows as a device instead of a browser
+  }
+});
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
